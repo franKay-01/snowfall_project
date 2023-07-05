aa10c5bf77d36419c9c5721a9b8086455dfaf990
Francis Kornu <fkay0450@gmail.com>
2023-07-05 22:26:28 +0000

    refact: fixing img warnings

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,27 +7,26 @@ import LinkedInLogo from '../assets/linkedIn.png'
 
 export default function Footer() {
   return (
-
-    <footer className="p-6 absolute w-full left-0 bottom-0 footer-color md:pt-16 md:pb-16 md:pl-60 md:pr-60">
+    <footer className="p-6 w-full left-0 bottom-0 footer-color md:pt-16 md:pb-16 md:pl-60 md:pr-60">
       <div className="container grid grid-cols-1 mx-auto gap-y-8 sm:grid-cols-3 md:grid-cols-4">
         <div className="flex flex-col space-y-4">
-          <img src={Logo}/>
+          <img src={Logo} alt="" />
         </div>
         <div className="flex flex-col space-y-4 md:ml-24">
           <div className="flex flex-col space-y-5 text-sm dark:text-gray-400">
-            <a rel="noopener noreferrer" className='nav-bar-text' href="#">Home</a>
-            <a rel="noopener noreferrer" className='nav-bar-text' href="#">Referral</a>
-            <a rel="noopener noreferrer" className='nav-bar-text' href="#">Team</a>
-            <a rel="noopener noreferrer" className='nav-bar-text' href="#">Blog</a>
+            <a rel="noopener noreferrer" className='nav-bar-text' href="/">Home</a>
+            <a rel="noopener noreferrer" className='nav-bar-text' href="/">Referral</a>
+            <a rel="noopener noreferrer" className='nav-bar-text' href="/">Team</a>
+            <a rel="noopener noreferrer" className='nav-bar-text' href="/">Blog</a>
           </div>
         </div>
         
         <div className="flex flex-col space-y-4">
           <div className="flex flex-col space-y-5 text-sm dark:text-gray-400">
-            <a rel="noopener noreferrer" className='nav-bar-text' href="#">Snowball Crypto User Agreement</a>
-            <a rel="noopener noreferrer" className='nav-bar-text' href="#">Cryptocurrency Risk Disclosure</a>
-            <a rel="noopener noreferrer" className='nav-bar-text' href="#">Privacy Policy</a>
-            <a rel="noopener noreferrer" className='nav-bar-text' href="#">Terms of Service</a>
+            <a rel="noopener noreferrer" className='nav-bar-text' href="/">Snowball Crypto User Agreement</a>
+            <a rel="noopener noreferrer" className='nav-bar-text' href="/">Cryptocurrency Risk Disclosure</a>
+            <a rel="noopener noreferrer" className='nav-bar-text' href="/">Privacy Policy</a>
+            <a rel="noopener noreferrer" className='nav-bar-text' href="/">Terms of Service</a>
           </div>
         </div>
         <div className="flex flex-col space-y-4">
@@ -35,27 +34,27 @@ export default function Footer() {
           <div className="flex flex-row text-sm md:ml-12">
             <div className="lg:block lg:w-auto mr-4">
               <a href='/get-started' className="brown-button button-margin-left">
-                <img src={TwitterLogo}/>
+                <img src={TwitterLogo} alt="" />
               </a>
             </div>
             <div className="lg:block lg:w-auto mr-4">
               <a href='/get-started' className="brown-button button-margin-left">
-                <img src={TelegramLogo}/>
+                <img src={TelegramLogo} alt="" />
               </a>
             </div>
             <div className="lg:block lg:w-auto mr-4">
               <a href='/get-started' className="brown-button button-margin-left">
-                <img src={InstagramLogo}/>
+                <img src={InstagramLogo} alt="" />
               </a>
             </div>
             <div className="lg:block lg:w-auto mr-4">
               <a href='/get-started' className="brown-button button-margin-left">
-                <img src={LinkedInLogo}/>
+                <img src={LinkedInLogo} alt="" />
               </a>
             </div>
             <div className="lg:block lg:w-auto">
               <a href='/get-started' className="brown-button button-margin-left">
-                <img src={FacebookLogo}/>
+                <img src={FacebookLogo} alt="" />
               </a>
             </div>
           </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,26 @@
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
-
+import BannerPhoneImg from "../assets/banner_phone.png"
 export default function Home(){
   return (
     <>
       <Navbar/>
-      <h1>HOME</h1>
+      <div className="home-banner grid justify-center">
+        <div className="container flex flex-row">
+          <div className="text-container flex flex-col">
+            <h1 className="banner-text">
+              Buy Crypto, <i>invest</i> in DeFi
+            </h1>
+            <h1 className="banner-sub-text">
+              Gain access to high-yield stablecoin vaults and generate interest in real-time via DeFi
+            </h1>
+          </div>
+          <div>
+            <img src={BannerPhoneImg} alt=""/>
+          </div>
+        </div>
+       
+      </div>
       <Footer/>
     </>
   )
